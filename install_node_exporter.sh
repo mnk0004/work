4377ffdb132c767ed0e4c8d2a6c32360ce4bdac5
mnk0004 <143609504+mnk0004@users.noreply.github.com>
2024-08-08 17:37:53 +0300

    Update install_node_exporter.sh

--- install_node_exporter.sh
+++ install_node_exporter.sh
@@ -11,6 +11,7 @@ mkdir /opt/node_exporter/
 echo "basic_auth_users:
   node_exporter: $PASS" > /opt/node_exporter/web.yml
 mkdir /opt/node_exporter/textfile_collector
+useradd --no-create-home --shell /bin/false node_exporter
 systemctl daemon-reload
 systemctl restart node_exporter.service
 systemctl status node_exporter.service
