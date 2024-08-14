671246c2d5ff7da4786b793f32ad21da68f2e94e
mnk0004 <143609504+mnk0004@users.noreply.github.com>
2024-08-14 15:06:15 +0300

    Update install_node_exporter.sh

--- install_node_exporter.sh
+++ install_node_exporter.sh
@@ -5,6 +5,7 @@ PASS=$2
 cd work-main
 curl -sLO https://github.com/prometheus/node_exporter/releases/download/v${NE_VER}/node_exporter-${NE_VER}.linux-amd64.tar.gz
 tar -zxf node_exporter-*.linux-amd64.tar.gz
+systemctl stop node_exporter.service
 cp node_exporter-*.linux-amd64/node_exporter /usr/local/bin/
 rm -rf node_exporter-*.linux-amd64/ && rm -f node_exporter-*.linux-amd64.tar.gz
 cp node_exporter.service /etc/systemd/system/
